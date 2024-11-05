807ec7c8812f8dd93b21f15ac819e3e54527ccb9
Richard McMichael <rmcmic10@outlook.com>
2024-11-05 17:17:49 +0000

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace BrightPaySolution
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
